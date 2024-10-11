8205c9d665582813d4f16efe75ea31d2844e886f
himdevops <himdevops5@gmail.com>
2024-10-10 21:02:53 -0600

    Update local-values.tf

--- terraform-manifest/actual-generic-vpc-module/local-values.tf
+++ terraform-manifest/actual-generic-vpc-module/local-values.tf
@@ -1,6 +1,8 @@
 # local value assigned name to an expression. That means we can use it multiple time 
 #within modules without repeating it. 
 # Local values are like a functions temporary local variable 
+# when we need to use complex expression multiple times we define it in local values.
+# use like locals.environment 
 locals {
   environment = var.my-environment
   owners      = var.team_divsion
